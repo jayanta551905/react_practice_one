59c8d3cc215758fccd43728deec7b41899942a55
Jayanta Mondal <jayanta551905@gmail.com>
2022-12-11 00:13:29 +0600

    create component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
+import About from "./components/about/About";
+import Home from "./components/home/Home";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home></Home>
+      <About></About>
     </div>
   );
 }
